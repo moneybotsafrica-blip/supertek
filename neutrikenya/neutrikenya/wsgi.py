f67b44834717da3ad611f17677142e66d73ec77c
moneybotsafrica-blip <moneybotsafrica@gmail.com>
2026-08-14 18:15:38 +0300

    Fix settings module path for Render deployment

--- neutrikenya/neutrikenya/wsgi.py
+++ neutrikenya/neutrikenya/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neutrikenya.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neutrikenya.neutrikenya.settings')
 
 application = get_wsgi_application()
